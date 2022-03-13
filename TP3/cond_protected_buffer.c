1d4ae2408f3cc4306695851e4d6da0c7e3df46f6
Bastien Morantin <bastien.morantin@telecom-paris.fr>
2022-03-13 15:16:19 +0100

    FIn partie 1.3, acces concurrents non bloquants

--- TP3/cond_protected_buffer.c
+++ TP3/cond_protected_buffer.c
@@ -76,14 +76,22 @@ void cond_protected_buffer_put(protected_buffer_t * b, void * d){
 // possible immedidately, return NULL. Otherwise, return the element.
 void * cond_protected_buffer_remove(protected_buffer_t * b){
   void * d;
-  
+
+  // Enter mutual exclusion
+  pthread_mutex_lock(&b->mutex);
 
   // Signal or broadcast that an empty slot is available in the
   // unprotected circular buffer (if needed)
+  if(circular_buffer_size(b->buffer) == b->buffer->max_size){
+    pthread_cond_signal(&b->emptyslot);
+  }
 
   d = circular_buffer_get(b->buffer);
   print_task_activity ("remove", d);
   
+  // Leave mutual exclusion
+  pthread_mutex_unlock(&b->mutex);
+
   return d;
 }
 
@@ -93,16 +101,19 @@ int cond_protected_buffer_add(protected_buffer_t * b, void * d){
   int done;
   
   // Enter mutual exclusion
-  //pthread_mutex_lock(&b->mutex);
+  pthread_mutex_lock(&b->mutex);
   // Signal or broadcast that a full slot is available in the
   // unprotected circular buffer (if needed)
+  if(circular_buffer_size(b->buffer)==0){
+    pthread_cond_signal(&b->fullslot);
+  }
 
   done = circular_buffer_put(b->buffer, d);
   if (!done) d = NULL;
   print_task_activity ("add", d);
 
   // Leave mutual exclusion
-  //pthread_mutex_unlock(&b->mutex);
+  pthread_mutex_unlock(&b->mutex);
   return done;
 }
 
@@ -115,7 +126,7 @@ void * cond_protected_buffer_poll(protected_buffer_t * b, struct timespec *absti
   int    rc = 0;
   
   // Enter mutual exclusio
-  //pthread_mutex_lock(&b->mutex);
+  pthread_mutex_lock(&b->mutex);
   // Wait until there is an empty slot to put data in the unprotected
   // circular buffer (circular_buffer_put) but waits no longer than
   // the given timeout.
@@ -127,7 +138,7 @@ void * cond_protected_buffer_poll(protected_buffer_t * b, struct timespec *absti
   print_task_activity ("poll", d);
 
   // Leave mutual exclusion
-  //pthread_mutex_unlock(&b->mutex);
+  pthread_mutex_unlock(&b->mutex);
   return d;
 }
 
